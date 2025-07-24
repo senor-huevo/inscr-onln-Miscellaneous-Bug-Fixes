833bd65c18ab38c5642d760442f7e2ef93364d9c
senor-huevo <137343393+senor-huevo@users.noreply.github.com>
2025-07-24 00:45:15 -0400

    Gem Dep and Gem Det (5) fix
    updated to be in the Great Renaming and with Stoat's requests for Gem Dep

--- scripts/classes/sigils/Gem Dependant.gd
+++ scripts/classes/sigils/Gem Dependant.gd
@@ -1,17 +1,21 @@
 extends SigilEffect
 
+func start_of_turn(cardAnim):
+	Gem_Dep()
+	
 # This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
 func handle_event(event: String, params: Array):
 
-	# Don't die in hand
-	if card.get_parent().name == "PlayerHand":
-		return
-
 	# attached_card_summoned represents the card bearing the sigil being summoned
-	if (event == "card_summoned" and params[0] == card) or (event == "card_perished" and params[0] != card):
+	# Left side is checking when the card is played, Right side is checking when anything perishes
+	if (event == "card_summoned" and params[0] == card and not card.in_hand) or (event == "card_perished" and params[0] != card and not card.in_hand and CardInfo.all_data.vanilla_gem_dep != true):
+			Gem_Dep()
 
-		# Die if no gems
-		if not "Perish" in card.get_node("AnimationPlayer").current_animation:
+func end_of_turn(cardAnim):
+	Gem_Dep()
+	
+func Gem_Dep():
+	if not "Perish" in card.get_node("AnimationPlayer").current_animation:
 
 			var kill = not (slotManager.get_friendly_cards_sigil("Great Mox") if is_friendly else slotManager.get_enemy_cards_sigil("Great Mox"))
 
@@ -24,4 +28,4 @@ func handle_event(event: String, params: Array):
 			if kill:
 				print("Gem dependant card should die!")
 				card.get_node("AnimationPlayer").play("Perish")
-		
+				
